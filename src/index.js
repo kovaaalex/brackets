40c3a6553443bb68349207a74666cbd001d1cbda
kovaaalex <kovalenko.alex04@gmail.com>
2024-08-31 13:18:36 +0300

    feat: fix similar open and close brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let firstBracket = []
+  let stack = []
+  let openAndClose = {}
+  bracketsConfig.forEach(el => { 
+    firstBracket.push(el[0])
+    openAndClose[el[1]] = el[0]
+  })
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === stack[stack.length - 1] && str[i] === openAndClose[str[i]]) stack.pop()
+    else if(firstBracket.includes(str[i])) stack.push(str[i])
+    else {
+      let lastBracketIndex = 0
+      bracketsConfig.forEach((el, index) => {
+        if(str[i] === el[1]) lastBracketIndex = index
+      })
+      if(bracketsConfig[lastBracketIndex][0] === stack[stack.length - 1]) stack.pop()
+      else return false
+    }
+  }
+  if (!stack.length) return true
+  else return false
 }
